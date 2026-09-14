a1418a2f9a02be3d8467a69273e10787ea153a3e
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-09-14 22:09:18 +0300

    Update AppDelegate.swift

--- AsasecIl2cppDumper-Gui-App/App/AppDelegate.swift
+++ AsasecIl2cppDumper-Gui-App/App/AppDelegate.swift
@@ -14,12 +14,16 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
+        options connectionOptions: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
 
-        return UISceneConfiguration(
+        let configuration = UISceneConfiguration(
             name: "Default Configuration",
             sessionRole: connectingSceneSession.role
         )
+
+        configuration.delegateClass = SceneDelegate.self
+
+        return configuration
     }
 }
