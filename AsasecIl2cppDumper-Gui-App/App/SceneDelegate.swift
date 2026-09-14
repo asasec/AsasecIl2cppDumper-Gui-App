bbc9190d89d5c9e2b63d9845ceb20c29ff9a1c84
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-09-14 22:15:51 +0300

    Update SceneDelegate.swift

--- AsasecIl2cppDumper-Gui-App/App/SceneDelegate.swift
+++ AsasecIl2cppDumper-Gui-App/App/SceneDelegate.swift
@@ -16,47 +16,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         let window = UIWindow(windowScene: windowScene)
 
-        let viewController = UIViewController()
-
-        viewController.view.backgroundColor = .systemRed
-
-        let label = UILabel()
-
-        label.text = """
-        ASASEC DEBUG
-
-        SCENE ÇALIŞIYOR
-
-        UIWindow oluşturuldu
-        UIViewController oluşturuldu
-        """
-
-        label.textColor = .white
-        label.font = UIFont.systemFont(
-            ofSize: 22,
-            weight: .bold
-        )
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
-
-        viewController.view.addSubview(label)
-
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(
-                equalTo: viewController.view.leadingAnchor,
-                constant: 20
-            ),
-
-            label.trailingAnchor.constraint(
-                equalTo: viewController.view.trailingAnchor,
-                constant: -20
-            ),
-
-            label.centerYAnchor.constraint(
-                equalTo: viewController.view.centerYAnchor
-            )
-        ])
+        let viewController = MainViewController()
 
         window.rootViewController = viewController
 
